f2db9c7eb7105507f85093558deeec907b393b3e
valenti94br <valenti94br@gmail.com>
2023-03-28 13:53:22 +0200

    Nuevo repositorio porque soy idiota y la he liado

---
+++ config/database.example.js
@@ -0,0 +1,17 @@
+const mysql = require('mysql2');
+
+const db = mysql.createConnection({
+
+host : 'localhost',
+
+user : 'tu usuario',
+
+password : 'tu contraseña',
+
+database:'nombre de la base de datos'
+
+});
+
+db.connect();
+
+module.exports = db;
