2a757d53d321b6790640ba17f641edc0084e03bd
valenti94br <valenti94br@gmail.com>
2023-03-29 00:03:35 +0200

    productos añadidos

--- index.js
+++ index.js
@@ -4,4 +4,67 @@ const PORT = 8080;
 const db = require("./config/database")
 
 app.use(express.json())
-app.listen(PORT, () => console.log(`Servidor levantado en el puerto ${PORT}`));
+app.listen(PORT, () => console.log(`Servidor levantado en el puerto ${8080}`));
+
+// Tabla de Products y Categories creada desde el código usando las transparencias
+app.get('/createtableProducts', (req, res) => {
+
+    let sql = 'CREATE TABLE products(id int AUTO_INCREMENT,title VARCHAR(255), body VARCHAR(255), PRIMARY KEY(id))'
+
+    db.query(sql, (err, result) => {
+
+        if (err) throw err;
+
+        console.log(result);
+
+        res.send('Products table created...')
+
+    })
+
+})
+
+app.get('/createtableCategories', (req, res) => {
+
+    let sql = 'CREATE TABLE categories(id int AUTO_INCREMENT,title VARCHAR(255), body VARCHAR(255), PRIMARY KEY(id))'
+
+    db.query(sql, (err, result) => {
+
+        if (err) throw err;
+
+        console.log(result);
+
+        res.send('Categories table created...')
+
+    })
+
+})
+
+const express = require("express");
+const app = express();
+const PORT = 8080;
+const db = require("./config/database");
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.listen(PORT, () => console.log(`Servidor levantado en el puerto ${8080}`));
+
+// Endpoint para añadir un producto nuevo
+app.post('/products', (req, res) => {
+
+    const { title, body } = req.body;
+
+    let sql = 'INSERT INTO products (title, body) VALUES (cartas yugioh, cartas magic)';
+
+    db.query(sql, [title, body], (err, result) => {
+
+        if (err) throw err;
+
+        console.log(result);
+
+        res.send('New product added...');
+
+    })
+
+})
